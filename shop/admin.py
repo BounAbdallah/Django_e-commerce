72d052d1a0bd661496f569eb612688f3e0b52d70
Bouna_Abdallah <bigmabo61345@gmail.com>
2023-03-26 15:05:33 +0000

    commit3

--- shop/admin.py
+++ shop/admin.py
@@ -3,6 +3,12 @@ from .models import Category, Product
 
 #Les models du categorie et du produit sont enregistrés ci dessous ! 
 
+admin.site.site_header = "DEEL_BII"
+admin.site.site_title = "Boutique Deel bii"
+admin.site.index_title ="Direction du site"
+
+
+
 
 admin.site.register(Category)
 admin.site.register(Product)
